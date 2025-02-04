1bac96f4db7d4e661498c96eacafcdd2440c7e7d
nitinsoni-coder <nitinsoni.ns090@gmail.com>
2025-02-04 11:13:25 +0530

    learned how to setup framer-motion and how to animate div

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { motion } from "motion/react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <motion.div
+        animate={{ x: 800, rotate: 360 }}
+        transition={{ duration: 1, delay: 0.5, repeat: Infinity }}
+        className="box"
+      ></motion.div>
+    </div>
+  );
+};
 
-export default App
+export default App;
