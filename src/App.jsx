c6cfda49b3bfd0b62906b28e07514384240a33e6
nitinsoni-coder <nitinsoni.ns090@gmail.com>
2025-02-04 11:30:35 +0530

    learned how to use drag property

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,19 @@ import { motion } from "motion/react";
 const App = () => {
   return (
     <div>
-      <motion.div
+      {/* <motion.div
         animate={{
           x: [0, 800, 800, 0, 0],
           y: [0, 0, 300, 300, 0],
           rotate: [0, 360, 0, -360, 0],
         }}
         transition={{ duration: 3, delay: 0.5, repeat: Infinity }}
+        className="box"
+      ></motion.div> */}
+      <motion.div
+        drag
+        whileDrag={{ scale: 0.8 }}
+        dragConstraints={{ left: 0, top: 0, right: "100%", bottom: "100%" }}
         className="box"
       ></motion.div>
     </div>
